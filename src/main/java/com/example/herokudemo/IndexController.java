97f4e58f40e12cff7d058b46c7f69ca6a6c86634
MrYepez0710 <57237395+MrYepez0710@users.noreply.github.com>
2020-01-07 15:50:23 -0500

    Update IndexController.java

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @GetMapping("/")
+    @GetMapping("/HolaMundo")
     public String index() {
         return "Hola mundo.";
     }
